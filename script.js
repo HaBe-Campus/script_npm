c2415987eb45708d66a88c767cf5990dfe5c550e
HaBe-Campus <hamid.berrahal@le-campus-numerique.fr>
2021-03-10 16:51:31 +0100

    compress image

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const imagemin = require('imagemin');
 const imageminPngquant = require('imagemin-pngquant');
 
 (async () => {
-    await imagemin(['images/*.jpg'], {
+    await imagemin(['images/*.png'], {
         destination: 'build/images',
         plugins: [
             imageminPngquant({ quality: [0, 0.05] })
